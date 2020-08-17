aa5ba5e20d6611d8bdf4f9048fd9e74da3940555
Denis Naughton <mjolinor75@hotmail.com>
2020-08-17 19:43:47 -0400

    Finished code(part 1)

--- lib/code.rb
+++ lib/code.rb
@@ -1,4 +1,6 @@
 class Code
+
+  attr_accessor :pegs
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -15,8 +17,37 @@ class Code
 
   def initialize(arr)
     @arr = arr
-    Code.valid_pegs?(arr)
+    if Code.valid_pegs?(arr)
+      @pegs = arr
+    else
+      raise "exception"
+    end
+  end
+
+  def self.random(len)
+    pegs = Array.new(len) { Code::POSSIBLE_PEGS.keys.sample}
+    Code.new(pegs)
+  end
+
+  def self.from_string(string)
+   Code.new(string.split(""))
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(guess)
+    (0...guess.length).count { |i| guess[i] == @pegs[i] }
+  end
+
+  def ==(code)
 
   end
+
 
 end
--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -2,4 +2,22 @@ require_relative "code"
 
 class Mastermind
 
+    def initialize(len)
+        
+        @secret_code = Code.random(len)
+    end
+
+    def print_matches(inst)
+
+    end
+
+    def ask_user_for_guess
+        print 'Enter a code'
+        input = gets.chomp
+
+
+    end
+
+
+
 end
